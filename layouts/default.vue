29b280cecbd75d7e0f162343968429aaef2d0b89
Adam Rolczyk <adamr6561@gmail.com>
2020-12-10 00:11:02 +0100

    homepage screen

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="app">
         <Toolbar />
-        <Nuxt />
+        <main class="main">
+            <Nuxt />
+        </main>
     </div>
 </template>
 
@@ -26,5 +28,15 @@ body {
     margin: 0;
     padding: 0;
     background: $white2;
+    background-image: url("~assets/tic-tac-toe.png");
+    background-repeat: repeat;
+    background-size: 40%;
+}
+
+.main {
+    margin-top: 56px;
+    width: 100%;
+    height: 100%;
+    position: relative;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,74 @@
 <template>
-    <div class="container">
-        <p>hello world</p>
+    <div class="homepage">
+        <div class="card">
+            <img class="card__image" src="~/assets/logo.png" alt="Logo" />
+            <p class="card__text">
+                Bugtrack is an awesome app for tracking bugs and assigning developers to fix them. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sagittis nunc ut iaculis
+                consequat. Vivamus faucibus sollicitudin fermentum. Praesent lorem eros, tempor sed sapien eget, eleifend vulputate sem. Sed venenatis, tortor ut feugiat dignissim, arcu odio
+                vestibulum ligula, id viverra dolor ex a eros. Curabitur eleifend, est eget tempus consectetur, ligula tellus pretium libero, lacinia placerat massa lacus at libero. Pellentesque ac mi
+                eget ligula fermentum aliquet. In hac habitasse platea dictumst.
+            </p>
+            <div class="card__buttons flex-wrapper">
+                <nuxt-link to="/login" class="card__button flex-wrapper">Start</nuxt-link>
+            </div>
+        </div>
     </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
+<style lang="scss">
+.homepage {
+    width: 100%;
+    height: calc(100vh - 56px);
+    position: relative;
+}
 
-export default Vue.extend({});
-</script>
+.card {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 500px;
+    height: auto;
+    background-color: $white1;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 150px;
+    padding: 32px;
+    box-sizing: border-box;
+    border-radius: 8px;
+    box-shadow: 0px 0px 23px -1px rgba(255, 127, 17, 0.1);
+
+    &__image {
+        width: 100%;
+        height: auto;
+    }
+    &__text {
+        font-family: $karla;
+        text-align: justify;
+        letter-spacing: 1px;
+        font-size: 16px;
+    }
+
+    &__buttons {
+        width: 100%;
+        height: auto;
+        margin-top: 32px;
+    }
+    &__button {
+        border: none;
+        width: 50%;
+        margin: 0 8px;
+        height: 32px;
+        border-radius: 8px;
+        background-color: $black;
+        font-family: $rubik;
+        color: $white1;
+        box-shadow: 0px 10px 13px -1px rgba(255, 127, 17, 0.1);
+        text-decoration: none;
+        &:hover {
+            background-color: $orange;
+            cursor: pointer;
+        }
+    }
+}
+</style>
