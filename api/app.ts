837bc5d5928ef9112bc31c2342f3c3efe104e7d7
Adam Rolczyk <adamr6561@gmail.com>
2020-12-11 02:07:18 +0100

    typescript target & module

--- api/app.ts
+++ api/app.ts
@@ -1,33 +1,26 @@
-const { ApolloServer, gql } = require("apollo-server-express");
-const { MongoClient } = require("mongodb");
-const express = require("express");
+import { ApolloServer, gql } from "apollo-server-express";
+import { MongoClient } from "mongodb";
+import resolvers from "../apollo/resolvers";
+import typeDefs from "../apollo/typeDefs";
+import express from "express";
 
-const client = new MongoClient(process.env.MONGODB_URI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-});
+const startServer = async (): Promise<void> => {
+    const app = express();
 
-const app = express();
+    // Apollo
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    server.applyMiddleware({ app });
 
-const typeDefs = gql`
-    type Query {
-        hello: String!
-    }
-`;
-const resolvers = {
-    Query: {
-        hello: () => "hello",
-    },
-};
+    // MongoDB
+    const client = new MongoClient(process.env.MONGODB_URI as string, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    });
+    await client.connect();
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
-
-server.applyMiddleware({ app });
-
-async function listDatabases(client: typeof MongoClient) {
     const databaseList = await client
         .db()
         .admin()
@@ -37,20 +30,10 @@ async function listDatabases(client: typeof MongoClient) {
     for (let db of databaseList.databases) {
         console.log(` - ${db.name}`);
     }
-}
 
-async function main() {
-    try {
-        await client.connect();
-        await listDatabases(client);
-    } catch (e) {
-        console.log(e);
-    } finally {
-        await client.close();
-    }
-}
+    app.listen({ port: 7000 }, () => {
+        console.log(`Server ready at http://localhost:7000${server.graphqlPath}`);
+    });
+};
 
-main().catch(console.error);
-// app.listen({ port: 4000 }, () => {
-//     console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
-// });
+startServer();
