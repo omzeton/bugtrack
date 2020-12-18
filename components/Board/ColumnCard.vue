cb4154dfd3045c034a0327ffe62d26893ce115f9
Adam Rolczyk <adamr6561@gmail.com>
2020-12-18 19:51:07 +0100

    card styles

--- components/Board/ColumnCard.vue
+++ components/Board/ColumnCard.vue
@@ -1,17 +1,17 @@
 <template>
-    <div v-if="cardData" class="column-card mb-4">
+    <div v-if="cardData" class="column-card mb-4 p-4 box-border cursor-pointer bg-black2 hover:bg-black3">
         <div class="column-card__title">
-            <h2>{{ cardData.title }}</h2>
+            <h2 class="text-white font-roboto text-xl font-bold mb-2">{{ cardData.title }}</h2>
         </div>
-        <div class="column-card__tag">
-            <p>{{ cardData.tag }}</p>
+        <div class="column-card__tag mt-2 mb-12">
+            <p class="column-card__tag-label px-4 text-sm text-white">{{ cardData.tag }}</p>
         </div>
-        <div class="column-card__bottom-section">
+        <div class="column-card__description mb-6">
+            <p class="text-nunito font-light text-white text-sm leading-6">test test test test test test test test tes ttes tet sat sdf asdf asdf</p>
+        </div>
+        <div class="column-card__bottom-section px-4 box-border">
             <div class="column-card__code">
-                <p>{{ cardData.code }}</p>
-            </div>
-            <div class="column-card__assignee">
-                <p>{{ cardData.assignee }}</p>
+                <p class="text-form font-nunito text-xs font-hairline">{{ cardData.code }}</p>
             </div>
         </div>
     </div>
@@ -28,12 +28,33 @@ export default class ColumnCard extends Vue {
 
 <style lang="scss" scoped>
 .column-card {
+    position: relative;
     width: 100%;
-    height: 200px;
-    background-color: $black3;
+    height: auto;
     display: block;
     overflow: hidden;
     border-radius: 8px;
     border: 1px solid $formWhite;
+    cursor: pointer;
+    &__tag {
+        display: block;
+        position: relative;
+        &-label {
+            position: absolute;
+            background: orange;
+            width: auto;
+            border-radius: 8px;
+            box-shadow: 0px 0px 13px -1px orange;
+        }
+    }
+    &__bottom-section {
+        position: absolute;
+        bottom: 12px;
+        left: 0;
+        width: 100%;
+        display: flex;
+        align-items: flex-start;
+        justify-content: space-between;
+    }
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
             colors: {
                 accent: "#0dff49",
                 form: "#56606b",
+                black1: "#000000",
+                black2: "#0e141b",
+                black3: "#101821",
+                white1: "#ffffff",
+                white2: "#fbf8f7",
+                white3: "#dddddd",
             },
         },
     },
